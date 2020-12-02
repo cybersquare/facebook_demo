09b32e1e6af58255e7a219c046e5ab215d7f7b5c
Cyber Square <70192724+cybersquare@users.noreply.github.com>
2020-12-02 10:21:37 +0530

    Add files via upload

---
+++ db/facebook_db.sql
@@ -0,0 +1,93 @@
+-- Facebook database design
+
+CREATE TABLE users(
+    pk_user_id INT PRIMARY KEY AUTO_INCREMENT, 
+    first_name VARCHAR(200) NOT NULL ,
+    last_name VARCHAR(200),
+    mobileno VARCHAR (30) UNIQUE,
+    email VARCHAR(100) UNIQUE,
+    dob DATE,
+    gender VARCHAR(10)
+    );
+
+CREATE TABLE login(
+    pk_login_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(200),
+    password VARCHAR(200),
+    fk_user_id INT,
+    FOREIGN KEY(fk_user_id) REFERENCES users(pk_user_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    );
+    
+CREATE TABLE friends(
+    pk_friends_id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_user_id INT,
+    fk_friends_id INT,
+    FOREIGN KEY(fk_user_id) REFERENCES users(pk_user_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(fk_friends_id) REFERENCES users(pk_user_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    );
+
+CREATE TABLE images(
+    pk_img_id INT PRIMARY KEY AUTO_INCREMENT,
+    post_id INT, 
+    img VARCHAR(100),
+    is_profile BOOLEAN,
+    FOREIGN KEY(post_id) REFERENCES psots(pk_post_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+CREATE TABLE posts(
+    pk_post_id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_user_id INT,
+    fk_image_id INT ,
+    content VARCHAR (400),
+    create_time DATETIME,
+    FOREIGN KEY(fk_user_id) REFERENCES users(pk_user_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(fk_image_id) REFERENCES images(pk_img_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+    );
+    
+CREATE TABLE comments(
+    pk_comment_id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_post_id INT,
+    comments VARCHAR (400),
+    FOREIGN KEY(fk_post_id) REFERENCES posts(pk_post_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+CREATE TABLE shares(
+    pk_share_id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_post_id INT,
+    fk_friends_id INT,
+    FOREIGN KEY(fk_post_id) REFERENCES posts(pk_post_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(fk_friends_id) REFERENCES friends(pk_friends_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+
+CREATE TABLE likes(
+    pk_like_id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_post_id INT,
+    fk_friends_id INT,
+    FOREIGN KEY(fk_post_id) REFERENCES posts(pk_post_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(fk_friends_id) REFERENCES friends(pk_friends_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+
+
+
+
+
+
+
+
+
+
+
+
